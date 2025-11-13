ffe6e96fdde00f934dd7349ba668372db374d7cd
kppb16 <krit.ppb.dev@gmail.com>
2025-11-13 17:59:21 +0700

    chore(nvim): display hidden and ignored files as default

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,6 +1,16 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    picker = {
+      hidden = true,
+      ignored = true, -- Use this for picker
+      sources = {
+        files = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
     image = {
       enabled = true,
       max_size = 5 * 1024 * 1024, -- Only render images under 5MB
